fa2b68dbd38d5787e5bf55e9ac4ca4c9e163d5ec
Adam Nyitrai <adam.nyitrai87@gmail.com>
2024-01-03 01:21:11 +0100

    quiz can be started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
-// Set of questions --> array of objects
-// Each question needs the following:
-// Question text
-// Set of answers
-// Which answer is correct
+//! Set of questions --> array of objects
+//! Each question needs the following:
+//! Question text
+//! Set of answers
+//! Which answer is correct
 
-// Landing page:
-// Explanation of the quiz
-// Start button
+//! Landing page:
+//! Explanation of the quiz
+//! Start button
 
 // Click the start button:
-// Landing page goes away
+//! Landing page goes away
 // Timer starts
 // The first question appears (with its answers)
 
@@ -36,52 +36,90 @@
 const questions = [
     {
         question: 'Inside which HTML element do we put the JavaScript?',
-        answers: ['<scripting>', '<script>', '<javascript>', '<js>'],
+        choices: ['<scripting>', '<script>', '<javascript>', '<js>'],
         correct: '<script>'
     },
     {
         question: 'Where is the correct place to insert a JavaScript?',
-        answers: ['The <head> section', 'The <body> section', 'Both the <head> section and the <body> section are correct'],
+        choices: ['The <head> section', 'The <body> section', 'Both the <head> section and the <body> section are correct'],
         correct: 'The <body> section'
     },
     {
         question: 'The external JavaScript file must contain the <script> tag.',
-        answers: ['True', 'False'],
+        choices: ['True', 'False'],
         correct: 'True'
     },
     {
         question: 'How do you write "Hello World" in an alert box?',
-        answers: ['msg("Hello World");', 'alert("Hello World");', 'akertBox("Hello World");', 'msgBox("Hello World");'],
+        choices: ['msg("Hello World");', 'alert("Hello World");', 'akertBox("Hello World");', 'msgBox("Hello World");'],
         correct: 'alert("Hello World");'
     },
     {
         question: 'How do you create a function in JavaScript?',
-        answers: ['function myFunction()', 'function = myFunction()', 'function:myFunction()'],
+        choices: ['function myFunction()', 'function = myFunction()', 'function:myFunction()'],
         correct: 'function myFunction()'
     },
     {
         question: 'How to write an IF statement in JavaScript?',
-        answers: ['if (i == 5)', 'if i = 5 then', 'if i == 5 then', 'if i = 5'],
+        choices: ['if (i == 5)', 'if i = 5 then', 'if i == 5 then', 'if i = 5'],
         correct: 'if (i == 5)'
     },
     {
         question: 'How does a WHILE loop start?',
-        answers: ['while i = 1 to 10', 'while (i <= 10; i++)', 'while (i <= 10)'],
+        choices: ['while i = 1 to 10', 'while (i <= 10; i++)', 'while (i <= 10)'],
         correct: 'while (i <= 10)'
     },
     {
         question: 'How does a FOR loop start?   ',
-        answers: ['for i = 1 to 5', 'for (i = 0; i <= 5', 'for (i = 0; i <= 5; i++)', 'for (i <= 5; i++)'],
+        choices: ['for i = 1 to 5', 'for (i = 0; i <= 5', 'for (i = 0; i <= 5; i++)', 'for (i <= 5; i++)'],
         correct: 'for (i = 0; i <= 5; i++)'
     },
     {
         question: 'How do you round the number 7.25, to the nearest integer?',
-        answers: ['rnd(7.25)', 'round(7.25)', 'Math.round(7.25)', 'Math.rnd(7.25)'],
+        choices: ['rnd(7.25)', 'round(7.25)', 'Math.round(7.25)', 'Math.rnd(7.25)'],
         correct: 'Math.round(7.25)'
     },
     {
         question: 'JavaScript is the same as Java.',
-        answers: ['True', 'False'],
+        choices: ['True', 'False'],
         correct: 'False'
     },
 ];
+
+const startBtn = document.getElementById('start');
+const startScreen = document.getElementById('start-screen');
+
+function startQuiz(e) {
+    e.preventDefault();
+    startScreen.remove();
+    document.getElementById('questions').setAttribute('class', 'start');
+    playingQuiz();
+}
+
+startBtn.addEventListener('click', startQuiz);
+
+function playingQuiz() {
+    const qTitle = document.getElementById('question-title');
+    const choicesList = document.getElementById('choices');
+    const feedbackMsg = document.getElementById('feedback');
+
+    for (let i = 0; i < questions.length; i++) {
+        qTitle.textContent = questions[i].question;
+        for (let j = 0; j < questions[i].choices.length; j++) {
+            const choiceBtn = document.createElement('button');
+            choiceBtn.textContent = questions[i].choices[j];
+            choiceBtn.dataset.choice = choiceBtn.textContent;
+            choicesList.append(choiceBtn);
+        }
+
+        choiceBtn.addEventListener('click', function () {
+            feedbackMsg.setAttribute('class', 'feedback');
+
+            if (choiceBtn.dataset.choice === questions[i].correct) {
+                feedbackMsg.innerHTML = 'True!';
+            } else {
+                feedbackMsg.innerHTML = 'Wrong!';
+            }
+        })
+    }
+}
